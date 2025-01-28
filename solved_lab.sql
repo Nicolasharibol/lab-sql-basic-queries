bf46d8b753e8543fba382ffe3079e498a80a1322
Nicolasharibol <nicolasmunoz086@gmail.com>
2025-01-28 15:31:12 +0100

    update

---
+++ solved_lab.sql
@@ -0,0 +1,44 @@
+use sakila;
+show tables;
+select * from actor;
+select * from film;
+select * from customer;
+select title
+from film;
+
+select name as language
+from language;
+
+select first_name
+from staff;
+
+select distinct release_year
+from film;
+
+select count(*) as number_of_stores
+from store;
+
+select count(*) as employees 
+from staff;
+
+select count(distinct inventory_id) 
+from inventory;
+select count( distinct rental_id)
+from rental;
+
+select count( distinct last_name) 
+from actor;
+
+SELECT title, length
+FROM film
+ORDER BY length DESC
+LIMIT 10;
+
+select * 
+from actor 
+where first_name = "Scarlett";
+
+select *
+from film
+where title like "%Armageddon%"
+	and length > 100;
